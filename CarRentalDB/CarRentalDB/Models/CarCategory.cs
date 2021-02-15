deeb48277af1281356e248dc15a42c356f072ba1
eitan <eitankatzenell@gmail.com>
2021-02-15 10:29:04 +0200

    second initial commit

---
+++ CarRentalDB/CarRentalDB/Models/CarCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace CarRentalDB.Models
+{
+    public partial class CarCategory
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Model { get; set; }
+        public bool Automatic { get; set; }
+    }
+}
---
+++ CarRentalDB/CarRentalDB/Models/CarRentalDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CarRentalDB.Models
+{
+    public partial class CarRentalDbContext : DbContext
+    {
+        public CarRentalDbContext()
+        {
+        }
+
+        public CarRentalDbContext(DbContextOptions<CarRentalDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<CarCategory> CarCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-ERBRHMC\\SQLEXPRESS;Initial Catalog=CarRentalDb;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Hebrew_CI_AS");
+
+            // FIXME: not sure if I should ommit this
+            //modelBuilder.Entity<CarCategory>(entity =>
+            //{
+            //    entity.Property(e => e.Id)
+            //        .ValueGeneratedNever()
+            //        .HasColumnName("ID");
+            //});
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
